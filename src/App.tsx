55219778cbd3b9e1eaeab8c510ebcb551cab82de
Ashmin Bhujel <ashminbhujel01@gmail.com>
2025-06-16 17:47:09 +0545

    feat: integrate Shadcn UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <h1>React State Management</h1>;
+  return (
+    <main className="container mx-auto flex flex-col gap-4 p-8">
+      <h1 className="text-center text-4xl font-bold">React State Management</h1>
+      <Button className="mx-auto w-fit">Hello World</Button>
+    </main>
+  );
 }
 
 export default App;
